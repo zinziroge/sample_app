8ff3a607829b8749988eb36f8f9f57e25e3fe85d
shin <zinziroge@gmail.com>
2014-03-16 12:07:42 +0900

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,3 +6,12 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+def sign_in(user)
+  fill_in "Email", with: user.email.upcase
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+
+  # Capybaraを使用していない場合にもサインインする。
+  cookies[:remember_token] = user.remember_token
+end

3ed83b2ad1d2961f91af62d18025bc0222dee3ab
shin <shin@Nataliya-VirtualBox.(none)>
2014-03-29 21:51:49 +0900

    add user microposts

--- spec/factories.rb
+++ spec/factories.rb
@@ -10,4 +10,10 @@ FactoryGirl.define do
     end
 
   end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
+
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,21 +13,38 @@ describe "Static pages" do
       should have_selector('h1', :text => 'Sample App')
     end
 
-	it "should have the base title" do
-      should have_selector('title',
-      :text => full_title(''))
+  	it "should have the base title" do
+        should have_selector('title',
+        :text => full_title(''))
     end
-
+  
     it "should not have a custom page title" do
       should_not have_selector('title',
         :text => "| Home")
     end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        visit signin_path
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
-  
+    
   describe "Help page" do
-	it "should have the content 'Help'" do
-		visit help_path
-		should have_selector('h1', :text => 'Help')
+  	it "should have the content 'Help'" do
+  		visit help_path
+  		should have_selector('h1', :text => 'Help')
     end
 
     it "should have the right title" do
@@ -35,11 +52,11 @@ describe "Static pages" do
       should have_selector('title', :text => full_title('Help'))
     end
   end
-
+  
   describe "About page" do
-	it "should have the content 'About Us'" do
-		visit about_path
-		should have_selector('h1', :text => 'About Us')
+  	it "should have the content 'About Us'" do
+  		visit about_path
+  		should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the right title" do
@@ -49,9 +66,9 @@ describe "Static pages" do
   end
 
   describe "Contact page" do
-	it "should have the content 'Contact Us'" do
-		visit contact_path
-		should have_selector('h1', :text => 'Contact')
+		it "should have the content 'Contact Us'" do
+			visit contact_path
+			should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the right title" do
